94f40cee69dd23c54879de24dab72c42de868f49
felipe konig <lipekonig@gmail.com>
2023-07-11 19:04:17 -0300

    First commit

---
+++ src/app/_app.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Ignite Shop',
+  description: '04 Projeto Ignite',
+}
+
+export default function MyApp({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
