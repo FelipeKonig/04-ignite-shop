d0e10876f84543426355b5e250d542ac070fdc55
felipe konig <lipekonig@gmail.com>
2023-07-13 17:09:49 -0300

    global css added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@
 import type { AppProps } from 'next/app'
 import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
+import { globalStyles } from '@/styles/global'
 
 const roboto = Roboto({
   weight: ['400', '700'],
@@ -12,6 +13,8 @@ export const metadata: Metadata = {
   title: 'Ignite Shop',
   description: '04 Projeto Ignite',
 }
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
