5369e849bd96108fea4951060faa77134685a1e3
felipe konig <lipekonig@gmail.com>
2023-07-14 18:25:02 -0300

    html and style  in product page added

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,28 @@
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '@/styles/pages/product'
 import { useRouter } from 'next/router'
 
 export default function Product() {
   const { query } = useRouter()
-  return <h1>Product: {JSON.stringify(query.id)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel sit
+          corrupti perspiciatis voluptatum! Aspernatur sed eos voluptatem
+          voluptate id quis qui fuga velit aperiam quibusdam optio vel a,
+          cupiditate sunt.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
