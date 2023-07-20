07422e974dfa521019ff2b33ca577745c70d8de0
felipe konig <lipekonig@gmail.com>
2023-07-20 00:47:34 -0300

    added cart div

--- src/pages/components/cart.tsx
+++ src/pages/components/cart.tsx
@@ -1,14 +1,86 @@
-import { Handbag } from 'phosphor-react'
-import { IconContainer } from '@/styles/pages/cart'
+import { X, Handbag } from 'phosphor-react'
+import {
+  CartContainer,
+  IconContainer,
+  ImageItem,
+  Info,
+  InfoContainer,
+  Item,
+  ItemContainer,
+  ItemDescription,
+} from '@/styles/pages/cart'
+import { useState } from 'react'
+import Image from 'next/image'
+
+import shirt from '../../assets/1.png'
 
 export default function Cart() {
+  const [openCart, setOpenCart] = useState<boolean>(false)
+
+  function changeOpenCart() {
+    if (openCart) {
+      setOpenCart(false)
+    } else {
+      setOpenCart(true)
+    }
+  }
+
   return (
     <>
-      <IconContainer>
+      <IconContainer onClick={changeOpenCart}>
         <Handbag size={24} color="gray" weight="bold" />
         {/* <Handbag size={24} color="white" weight="bold" />
         <span>5</span> */}
       </IconContainer>
+      {openCart && (
+        <CartContainer>
+          <X size={24} color="gray" weight="bold" onClick={changeOpenCart} />
+          <h2>Sacola de compras</h2>
+          <ItemContainer>
+            <Item>
+              <ImageItem>
+                <Image src={shirt} alt="" />
+              </ImageItem>
+              <ItemDescription>
+                <p>Camiseta Beyond the Limits</p>
+                <h3>R$ 79,90</h3>
+                <button>Remover</button>
+              </ItemDescription>
+            </Item>
+            <Item>
+              <ImageItem>
+                <Image src={shirt} alt="" />
+              </ImageItem>
+              <ItemDescription>
+                <p>Camiseta Beyond the Limits</p>
+                <h3>R$ 79,90</h3>
+                <button>Remover</button>
+              </ItemDescription>
+            </Item>
+            <Item>
+              <ImageItem>
+                <Image src={shirt} alt="" />
+              </ImageItem>
+              <ItemDescription>
+                <p>Camiseta Beyond the Limits</p>
+                <h3>R$ 79,90</h3>
+                <button>Remover</button>
+              </ItemDescription>
+            </Item>
+          </ItemContainer>
+          <InfoContainer>
+            <Info>
+              <p>Quantidade</p>
+              <p>3 itens</p>
+            </Info>
+            <Info>
+              <h3>Valor total</h3>
+              <h3>R$ 270,00</h3>
+            </Info>
+            <button>Finalizar compra</button>
+          </InfoContainer>
+        </CartContainer>
+      )}
     </>
   )
 }
--- src/styles/pages/cart.ts
+++ src/styles/pages/cart.ts
@@ -26,3 +26,124 @@ export const IconContainer = styled('button', {
     cursor: 'initial',
   },
 })
+
+export const CartContainer = styled('main', {
+  display: 'block',
+
+  position: 'fixed',
+  height: '100vh',
+  width: '30rem',
+  padding: '2rem',
+  float: 'right',
+  background: '$gray800',
+  right: 0,
+  top: 0,
+  'z-index': 9999,
+  transition: 'transform 0.3s ease',
+
+  svg: {
+    float: 'right',
+    cursor: 'pointer',
+  },
+
+  h2: {
+    marginTop: '4rem',
+    clear: 'both',
+  },
+})
+
+export const ItemContainer = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  gap: '1.5rem',
+
+  'overflow-x': 'auto',
+  marginTop: '3rem',
+  maxHeight: 436,
+})
+
+export const Item = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '1.25rem',
+  alignSelf: 'stretch',
+})
+
+export const ImageItem = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  width: '100%',
+  borderRadius: 8,
+  padding: '0.25rem',
+  maxWidth: '5.93rem',
+  height: '5.93rem',
+  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+
+  img: {
+    width: '100%',
+    height: 'auto',
+    objectFit: 'cover',
+  },
+})
+
+export const ItemDescription = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  gap: '0.5rem',
+  flex: '1 0 0',
+
+  button: {
+    display: 'block',
+    paddingTop: 15,
+
+    color: '$green500',
+    background: 'transparent',
+    border: 0,
+    fontWeight: 'bold',
+    fontSize: '1rem',
+    cursor: 'pointer',
+
+    '&:hover': {
+      color: '$green300',
+    },
+  },
+})
+
+export const InfoContainer = styled('div', {
+  position: 'absolute',
+  bottom: '3rem',
+  width: '80%',
+
+  button: {
+    width: '100%',
+    marginTop: '3rem',
+    backgroundColor: '$green500',
+    border: 0,
+    color: '$white',
+    borderRadius: 8,
+    padding: '1.25rem',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+    fontSize: '$md',
+    fontFamily: '__Roboto_b4911f',
+
+    '&:disabled': {
+      opacity: 0.6,
+      cursor: 'not-allowed',
+    },
+
+    '&:not(:disabled):hover': {
+      backgroundColor: '$green300',
+    },
+  },
+})
+
+export const Info = styled('div', {
+  display: 'flex',
+  justifyContent: 'space-between',
+  marginBottom: '1rem',
+})
